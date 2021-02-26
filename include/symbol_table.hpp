0169899225f0f4260de89832f7b02f9d2a494599
Pavel Kovalenko <pskovalenko@protonmail.com>
2021-02-27 00:55:35 +0700

    rework classes SymbolTable and Procedure

--- include/symbol_table.hpp
+++ include/symbol_table.hpp
@@ -1,36 +1,114 @@
 #pragma once
 
 #include "parser.hpp"
-#include <map>
+#include <unordered_map>
+#include <unordered_set>
 
 class Procedure;
 
-class SymbolTable {
+namespace std {
 
-private:
-    std::map<nodes::Ident, nodes::Expression> consts;
-    std::map<nodes::Ident, nodes::Type> types;
-    std::map<nodes::Ident, nodes::Type> variables;
-    std::map<nodes::Ident, Procedure> procedures;
+template<>
+struct hash<nodes::Ident> {
+    std::size_t operator()(nodes::Ident const& id) const noexcept {
+        return std::hash<std::string_view>{}(std::string_view(id.data(), id.size()));
+    }
 };
 
-class Procedure {
-    nodes::Ident name;
-    nodes::ProcedureType type;
-    SymbolTable symbols;
-    nodes::StatementSequence body;
-    std::optional<nodes::ExpressionPtr> ret;
-};
+} // namespace std
 
 class Module;
 
 struct Import {
     nodes::Ident name;
-    Module* ptr;
+    std::unique_ptr<Module> table;
 };
 
-class ModuleTable : private SymbolTable {
-    std::map<nodes::Ident, Import> imports;
+enum class SymbolGroup {
+    TYPE, VAR, CONST
+};
+
+struct Symbol {
+    nodes::Ident name;
+    SymbolGroup group;
+    nodes::TypePtr type;
+    CodePlace place;
+    size_t count;
+};
+
+template <class T>
+using SymbolMap = std::unordered_map<nodes::Ident, T>;
+
+using SymbolSet = std::unordered_set<nodes::Ident>;
+
+class SymbolTable;
+
+using TablePtr = std::shared_ptr<SymbolTable>;
+
+using SymbolResult = SemResult<Symbol>;
+
+class SymbolTable {
+public:
+    SymbolTable();
+    virtual ~SymbolTable() {}
+    Error parse(const nodes::DeclarationSequence&);
+
+    virtual SemResult<Symbol> get_symbol(const nodes::QualIdent& ident, CodePlace place) const;
+    virtual SemResult<nodes::ExpressionPtr> get_value(const nodes::QualIdent& ident, CodePlace place) const;
+    virtual SemResult<TablePtr> get_table(const nodes::QualIdent& ident, CodePlace place) const;
+
+    bool has_symbol(const nodes::QualIdent& ident) const { return !get_symbol(ident, CodePlace()); }
+
+    virtual Error add_symbol(nodes::IdentDef ident, SymbolGroup group, nodes::TypePtr type, CodePlace place);
+    Error add_value(nodes::IdentDef ident, SymbolGroup group, nodes::TypePtr type, CodePlace place, nodes::ExpressionPtr value);
+    Error add_table(nodes::IdentDef ident, SymbolGroup group, nodes::TypePtr type, CodePlace place, TablePtr table);
+private:
+    SymbolMap<Symbol> symbols;
+    SymbolMap<nodes::ExpressionPtr> values;
+    SymbolMap<TablePtr> tables;
+    nodes::StatementSequence body;
+};
+
+class Procedure : public SymbolTable {
+public:
+    Procedure(nodes::Ident name, nodes::ProcedureType type, std::optional<nodes::ExpressionPtr> ret, nodes::StatementSequence body, SymbolTable* parent)
+        :  SymbolTable(), m_name(name), m_type(type), m_ret(ret), m_body(body), m_parent(parent) {}
+
+    virtual SemResult<Symbol> get_symbol(const nodes::QualIdent& ident, CodePlace place) const override {
+        if (ident.qual) {
+            return m_parent->get_symbol(ident, place);
+        } else {
+            return SymbolTable::get_symbol(ident, place);
+        }
+    }
+    virtual SemResult<nodes::ExpressionPtr> get_value(const nodes::QualIdent& ident, CodePlace place) const override {
+        if (ident.qual) {
+            return m_parent->get_value(ident, place);
+        } else {
+            return SymbolTable::get_value(ident, place);
+        }
+    }
+    virtual SemResult<TablePtr> get_table(const nodes::QualIdent& ident, CodePlace place) const override {
+        if (ident.qual) {
+            return m_parent->get_table(ident, place);
+        } else {
+            return SymbolTable::get_table(ident, place);
+        }
+    }
+
+    virtual Error add_symbol(nodes::IdentDef ident, SymbolGroup group, nodes::TypePtr type, CodePlace place) override {
+        if (ident.def) {
+            return ErrorBuilder(place).format("Cannot export local variable {}", ident.ident).build();
+        } else {
+            return SymbolTable::add_symbol(ident, group, type, place);
+        }
+    }
+private:
+    nodes::Ident m_name;
+    nodes::ProcedureType m_type;
+    std::optional<nodes::ExpressionPtr> m_ret;
+    nodes::StatementSequence m_body;
+    SymbolTable* m_parent;
 };
 
 class Module {};
---
+++ src/symbol_table.cpp
@@ -0,0 +1,122 @@
+#include "symbol_table.hpp"
+
+SymbolTable::SymbolTable() {}
+
+Error SymbolTable::parse(const nodes::DeclarationSequence& seq) {
+    for (auto& decl : seq.constDecls) {
+        if (auto type = decl.expression->get_type(*this); type) {
+            auto error = add_value(decl.ident, SymbolGroup::CONST, type.get_ok(), decl.expression->place, decl.expression);
+            if (error) return error;
+        } else {
+            return type.get_err();
+        }
+    }
+    for (auto& decl : seq.typeDecls) {
+        if (auto typeError = decl.type->check(*this); typeError) {
+            return typeError;
+        } else {
+            auto error = add_symbol(decl.ident, SymbolGroup::TYPE, decl.type, decl.type->place);
+            if (error) return error;
+        }
+    }
+    for (auto& decl : seq.variableDecls) {
+        if (auto typeError = decl.type->check(*this); typeError) {
+            return typeError;
+        } else {
+            for (auto& var : decl.list) {
+                auto error = add_symbol(var, SymbolGroup::VAR, decl.type, decl.type->place);
+                if (error) return error;
+            }
+        }
+    }
+    for (auto& _decl : seq.procedureDecls) {
+        auto& decl = *dynamic_cast<nodes::ProcedureDeclaration*>(_decl.get());
+        if (auto typeError = decl.type.check(*this); typeError) {
+            return typeError;
+        } else {
+            auto table = std::make_shared<Procedure>(decl.name.ident, decl.type, decl.ret, decl.body, this);
+            if (auto tableError = table->parse(decl.decls); tableError) {
+                return tableError;
+            }
+            nodes::TypePtr typePtr = std::make_shared<nodes::ProcedureType>(decl.type);
+            auto error = add_table(decl.name, SymbolGroup::CONST, typePtr, decl.place, TablePtr(table));
+            if (error) return error;
+        }
+    }
+    return {};
+}
+
+Error SymbolTable::add_symbol(nodes::IdentDef ident, SymbolGroup group, nodes::TypePtr type, CodePlace place) {
+    if (symbols.contains(ident.ident)) {
+        auto symbol = symbols[ident.ident];
+        return ErrorBuilder(place).format("Redefinition of symbol {}", ident.ident)
+            .format("{} First definition here", symbol.place)
+            .format("{} Second definition here", place).build();
+    } else {
+        Symbol symbol;
+        symbol.name = ident.ident;
+        symbol.group = group;
+        symbol.type = type;
+        symbol.place = place;
+        symbol.count = 0;
+        symbols[ident.ident] = symbol;
+        return {};
+    }
+}
+
+Error SymbolTable::add_value(nodes::IdentDef ident, SymbolGroup group, nodes::TypePtr type, CodePlace place,
+                             nodes::ExpressionPtr value) {
+    if (auto error = add_symbol(ident, group, type, place); error) {
+        return error;
+    } else {
+        values[ident.ident] = value;
+        return {};
+    }
+}
+
+Error SymbolTable::add_table(nodes::IdentDef ident, SymbolGroup group, nodes::TypePtr type, CodePlace place, TablePtr table) {
+    if (auto error = add_symbol(ident, group, type, place); error) {
+        return error;
+    } else {
+        tables[ident.ident] = std::move(table);
+        return {};
+    }
+}
+
+SemResult<Symbol> SymbolTable::get_symbol(const nodes::QualIdent& ident, CodePlace place) const {
+    auto error =  ErrorBuilder(place).format("Symbol {} not found", place).build();
+    if (ident.qual) {
+        return error;
+    } else {
+        if (auto res = symbols.find(ident.ident); res != symbols.end()) {
+            return Symbol(res->second);
+        } else {
+            return error;
+        }
+    }
+}
+
+SemResult<nodes::ExpressionPtr> SymbolTable::get_value(const nodes::QualIdent& ident, CodePlace place) const {
+    auto error =  ErrorBuilder(place).format("Symbol {} not found", place).build();
+    if (ident.qual) {
+        return error;
+    } else {
+        if (auto res = values.find(ident.ident); res != values.end()) {
+            return nodes::ExpressionPtr(res->second);
+        } else {
+            return error;
+        }
+    }
+}
+SemResult<TablePtr> SymbolTable::get_table(const nodes::QualIdent& ident, CodePlace place) const {
+    auto error =  ErrorBuilder(place).format("Symbol {} not found", place).build();
+    if (ident.qual) {
+        return error;
+    } else {
+        if (auto res = tables.find(ident.ident); res != tables.end()) {
+            return TablePtr(res->second);
+        } else {
+            return error;
+        }
+    }
+}
